c8f45b3752a733089fe3cf529862b88fe38583e9
viviniko <viviniko.shop@gmail.com>
2017-12-30 18:23:38 +0800

    contract

--- src/Contracts/ImageService.php
+++ src/Contracts/ImageService.php
@@ -5,16 +5,30 @@ namespace Viviniko\Media\Contracts;
 interface ImageService
 {
     public function search($keywords);
+
+    public function getUrl($id);
+
     /**
      * Save a new entity in repository
      *
-     * @param mixed $file
-     * @param int $width
-     * @param int $height
-     *
+     * @param $file
+     * @param string $dir
+     * @param null $width
+     * @param null $height
+     * @param int $quality
      * @return mixed
      */
-    public function save($file, $width = null, $height = null);
+    public function save($file, $dir = 'default', $width = null, $height = null, $quality = 75);
+
+    /**
+     * @param $id
+     * @param $width
+     * @param $height
+     * @param null $x
+     * @param null $y
+     * @return mixed
+     */
+    public function crop($id, $width, $height, $x = null, $y = null);
 
     /**
      * Delete a entity in repository by id
