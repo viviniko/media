78033dee731e485810974918e1d5eaf63dbef22c
viviniko <viviniko.shop@gmail.com>
2019-05-17 05:17:20 +0800

    :trollface:

--- src/Repositories/FileRepository.php
+++ src/Repositories/FileRepository.php
@@ -2,50 +2,9 @@
 
 namespace Viviniko\Media\Repositories;
 
-use Viviniko\Repository\SearchRequest;
+use Viviniko\Repository\CrudRepository;
 
-interface FileRepository
+interface FileRepository extends CrudRepository
 {
-    /**
-     * Search.
-     *
-     * @param SearchRequest $searchRequest
-     *
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Collection
-     */
-    public function search(SearchRequest $searchRequest);
-
-    /**
-     * @param $id
-     * @param $columns
-     * @return mixed
-     */
-    public function find($id, $columns = ['*']);
-
-    /**
-     * @param $column
-     * @param null $value
-     * @param array $columns
-     * @return mixed
-     */
-    public function findBy($column, $value = null, $columns = ['*']);
-
-    /**
-     * @param array $data
-     * @return mixed
-     */
-    public function create(array $data);
-
-    /**
-     * @param null $column
-     * @param null $value
-     * @return mixed
-     */
-    public function count($column = null, $value = null);
-
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function delete($id);
+    
 }
